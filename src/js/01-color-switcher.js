7ba26e511eb038ea3cb48db00fdd9570748d8ac7
Artem <artic12101997dr@gmail.com>
2022-12-26 18:28:31 +0200

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const buttonChangerStart = document.querySelector("button[data-start]")
+const buttonChangerStop = document.querySelector("button[data-stop]");
+let changeTime = null;
+
+
+function handleChangeColorStart() {
+	if (buttonChangerStart.getAttribute("data-start") !== "") {
+		alert("Already running");
+		return
+	}
+	changeTime = setInterval(() => {
+		let color = getRandomHexColor();
+		document.body.style.backgroundColor = color;
+		console.log(color);
+	}, 1000)
+	buttonChangerStart.setAttribute("data-start", "active");
+	// Другий варіант як в дз щоб кнопка була неактивна
+	// buttonChangerStart.setAttribute("disabled", true);
+}
+
+function handleChangeColorStop() {
+	clearInterval(changeTime);
+	document.body.style.backgroundColor = '#FFFFFF';
+	console.log("Color changing was stopped and set to default");
+}
+
+buttonChangerStart.addEventListener("click", handleChangeColorStart)
+buttonChangerStop.addEventListener("click", handleChangeColorStop)
